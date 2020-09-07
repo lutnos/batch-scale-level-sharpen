f377b320496d68da771d5a45d623553f94e3bf26
Dave English <4254983+lutnos@users.noreply.github.com>
2020-09-07 15:55:49 +0100

    Static NEF works

--- entry.sh
+++ entry.sh
@@ -4,6 +4,5 @@ Xvfb :100 -ac &
 sleep 5
 echo Off we go
 echo $*
-DISPLAY=:100 gimp -i -f -d -s -b "'""$*""'" -b '(gimp-quit 0)'
-tail -f /dev/null
+DISPLAY=:100 gimp -i -f -d -s -b  '(script-fu-batch-sls "/data/*.NEF" TRUE "_h.jpg" 0.50 FALSE TRUE FALSE FALSE FALSE)' -b '(gimp-quit 0)'
 echo "Done"
